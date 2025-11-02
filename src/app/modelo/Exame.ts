77a54e2d17122bae4fba746ec88661715c4b7c7b
JuliaRDiniz <juliarodriguesdiniz@hotmail.com>
2025-11-02 18:55:43 -0300

    ajuste no modelo Exame

--- src/app/modelo/Exame.ts
+++ src/app/modelo/Exame.ts
@@ -3,9 +3,9 @@ import { Paciente } from './Paciente';
 export class Exame {
   id?: string;
   patientId: string;
-  modality: string; // CT, US, MR, etc.
-  procedure: string; // Descrição do exame
+  modality: string;
+  procedure: string;
   performedAt: string;
   createdAt?: string;
-  patient?: Paciente; // Dados do paciente
+  patient?: Paciente;
 }
