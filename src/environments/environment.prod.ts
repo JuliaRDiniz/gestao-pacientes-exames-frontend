3e8ccc9ca0a3a023a8d25e3fc7ca83e23c5a9e3f
JuliaRDiniz <juliarodriguesdiniz@hotmail.com>
2025-11-04 23:26:11 -0300

    ajustes de responsividade e atualização de estilos

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://gestao-pacientes-exames-backend.onrender.com',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'https://gestao-pacientes-exames-backend.onrender.com',
+};
